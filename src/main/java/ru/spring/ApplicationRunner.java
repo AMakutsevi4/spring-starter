9c6f43f9b4f839ca651bd051ce826f07ec8bf90b
AMakutsevi4 <amakutsevich@bk.ru>
2022-08-17 10:59:46 +0900

    Factory method injection

--- src/main/java/ru/spring/ApplicationRunner.java
+++ src/main/java/ru/spring/ApplicationRunner.java
@@ -2,11 +2,14 @@ package ru.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.spring.database.pool.ConnectionPool;
+import ru.spring.database.repository.CompanyRepository;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
         System.out.println(context.getBean("pool1", ConnectionPool.class));
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
--- src/main/java/ru/spring/database/repository/CompanyRepository.java
+++ src/main/java/ru/spring/database/repository/CompanyRepository.java
@@ -1,12 +1,18 @@
 package ru.spring.database.repository;
 
+import lombok.Value;
 import ru.spring.database.pool.ConnectionPool;
 
+@Value
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
+    ConnectionPool connectionPool;
 
     public CompanyRepository(ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
+    }
+
+    public static CompanyRepository of(ConnectionPool connectionPool) {
+        return new CompanyRepository(connectionPool);
     }
 }
