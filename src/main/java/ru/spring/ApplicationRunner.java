4680794b518a060a8950971bef8d024188672169
AMakutsevi4 <amakutsevich@bk.ru>
2022-08-17 09:57:31 +0900

    Constructor injection

--- src/main/java/ru/spring/ApplicationRunner.java
+++ src/main/java/ru/spring/ApplicationRunner.java
@@ -7,6 +7,6 @@ public class ApplicationRunner {
 
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean("pool2", ConnectionPool.class));
+        System.out.println(context.getBean("pool1", ConnectionPool.class));
     }
-    }
+}
--- src/main/java/ru/spring/database/pool/ConnectionPool.java
+++ src/main/java/ru/spring/database/pool/ConnectionPool.java
@@ -1,4 +1,17 @@
 package ru.spring.database.pool;
 
+import lombok.Data;
+import lombok.Value;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Value
 public class ConnectionPool {
+    String username;
+    Integer poolSize;
+    List<Object> args;
+    Map<String, Object> properties;
+
 }
