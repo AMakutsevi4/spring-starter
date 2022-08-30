8ca17a31375c413e90bbf20dbd3d1b3facfc2746
AMakutsevi4 <amakutsevich@bk.ru>
2022-08-30 17:53:32 +0900

    bean part1

--- src/main/java/ru/spring/config/ApplicationConfiguration.java
+++ src/main/java/ru/spring/config/ApplicationConfiguration.java
@@ -1,9 +1,13 @@
 package ru.spring.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import ru.spring.database.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import ru.spring.database.pool.ConnectionPool;
+import ru.spring.database.repository.CrudRepository;
+import ru.spring.database.repository.UserRepository;
 import ru.web.config.WebConfiguration;
 
 /*@ImportResource("classpath:application.hml")*/
@@ -13,9 +17,20 @@ import ru.web.config.WebConfiguration;
 @ComponentScan(basePackages = "ru.spring",
         useDefaultFilters = false,
         includeFilters = {
-        @Filter(type = FilterType.ANNOTATION, value = Component.class),
-        @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
-        @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
         })
 public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
 }
