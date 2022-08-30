b4e2680c89240538e1d568397b45b70a3898e8e5
AMakutsevi4 <amakutsevich@bk.ru>
2022-08-30 17:37:58 +0900

    import & importResource

--- src/main/java/ru/spring/config/ApplicationConfiguration.java
+++ src/main/java/ru/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,13 @@
 package ru.spring.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import ru.spring.database.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import ru.web.config.WebConfiguration;
 
+/*@ImportResource("classpath:application.hml")*/
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "ru.spring",
