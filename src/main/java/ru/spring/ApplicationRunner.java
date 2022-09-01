111d8bc2eee58e480493fbcbf1fd779f66bfad91
AMakutsevi4 <amakutsevich@bk.ru>
2022-09-01 17:58:10 +0900

    profiles

--- src/main/java/ru/spring/ApplicationRunner.java
+++ src/main/java/ru/spring/ApplicationRunner.java
@@ -10,6 +10,8 @@ public class ApplicationRunner {
     public static void main(String[] args) {
 
         try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
+            context.getEnvironment().setActiveProfiles("web", "prod");
+            context.refresh();
             System.out.println(context.getBean("pool1", ConnectionPool.class));
             var companyRepository = context.getBean("companyRepository", CrudRepository.class);
             System.out.println(companyRepository.findById(1));
